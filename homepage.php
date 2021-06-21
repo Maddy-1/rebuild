6f456e850e550ded1454219414b6c56968c1a53d
Maddy <maddy.flynn@icloud.com>
2021-06-21 22:20:31 +0100

    Nav bar formatted

--- homepage.php
+++ homepage.php
@@ -8,10 +8,14 @@
             <a href="homepage.php" style="float:left;border-right:2px solid;
     ">Welcome User</a>
             <a href="homepage.php" style="float:right;border-left:2px solid;
-    ">Log Out</a>
+    ">Log out</a>
         </div>
         
-        <div class = "main"></div>
+        <div class = "main">
+            <div class="main-section"></div>
+            <div class="main-section"></div>
+            <div class="main-section"></div>
+        </div>
     </body>
 </html>
 <? // access by localhost/rebuild/homepage.php?>
