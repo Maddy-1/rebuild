952c6782e1bafe71fcbbfd830245df107150aec5
Maddy <maddy.flynn@icloud.com>
2021-06-25 16:33:44 +0100

    Header and nav bar repaired

--- homepage.php
+++ homepage.php
@@ -5,17 +5,18 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="style.css">
+<title>The Adventures of You</title>
 </head>
 <body>
 
 <div class="header">
-  <h1>Header</h1>
+  <h1>The Adventures of You</h1>
 </div>
 
 <div class="topnav">
-  <a href="#">Link</a>
-  <a href="#">Link</a>
-  <a href="#">Link</a>
+  <a href="#" style="float:left; border-right:3px solid;">Welcome User</a>
+  <a href="#" style= "float: right; border-left: 3px solid;">Log out</a>
+
 </div>
 
 </body>
