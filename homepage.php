2b2ca9f77a9883edcac204ddf52450e3e7085cd7
Maddy <maddy.flynn@icloud.com>
2021-06-25 16:10:04 +0100

    restart basic

--- homepage.php
+++ homepage.php
@@ -1,21 +1,23 @@
-<html>
-    <title>The Adventures of you</title>
-    <head>
-    <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <div class = "header">
-            <a href="homepage.php" style="float:left;border-right:2px solid;
-    ">Welcome User</a>
-            <a href="homepage.php" style="float:right;border-left:2px solid;
-    ">Log out</a>
-        </div>
-        
-        <div class = "main">
-            <div class="main-section"></div>
-            <div class="main-section"></div>
-            <div class="main-section"></div>
-        </div>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>CSS Website Layout</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>Header</h1>
+</div>
+
+<div class="topnav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+</div>
+
+</body>
 </html>
 <? // access by localhost/rebuild/homepage.php?>
