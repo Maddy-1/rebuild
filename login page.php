d5ad1098f75f11919d61b4614a40d90e672d9ac9
Maddy <maddy.flynn@icloud.com>
2021-07-01 11:35:51 +0100

    upload designs and started login page

---
+++ login page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Registration</title>
+  <link rel="stylesheet" type="text/css" href="style.css"> 
+</head>
+<body>
+  <div class ="header">
+    <h1>The Adventures of You</h1>
+  </div>
+  <div class ="box">
+    <div class ="header">
+      <h1>Login</h1>
+    </div>
+    <h5>Username</h5>
+    <div class = "inputbox"></div>
+    <h5>Password</h5>
+    <div class = "inputbox"></div>
+    <div class ="signup"></div>
+    <div class = "loginbutton"></div>
+  </div>
+  
+</body>
+</html>
