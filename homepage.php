066228d22fca8e6900d9b2fb83ad118e93682ef0
Maddy <maddy.flynn@icloud.com>
2021-07-06 23:28:30 +0100

    New post button as button

--- homepage.php
+++ homepage.php
@@ -20,21 +20,19 @@
 <div class = "content">
   <div class = "row">
     <div class = "column left">
-      <div class = "box">
+      <div class = "box" style="background-color:#e9e9e9;">
         <div class = "header" style = "padding:0px; margin:0px;"><h4>Past Adventures</h4></div>
-        <div class ="box"style="margin:0px; margin-top:5px;">
-          <div class="box"><p>Map</p></div>
+        <div class ="box"style="margin:0px; margin-top:5px;background-color:#f1f1f1;">
+          <div class="box"style="background-color:#766;"><p>Map</p></div>
         </div>
       </div>
-    <div class = "postbutton">
-      <p>Record a new memory</p>
-    </div>
+    <button class = "postbutton">Record a new memory</button>
     </div>
     
     <div class = "column right">
-      <div class="box">
+      <div class="box"style="background-color:#e9e9e9;">
         <div class = "header" style = "padding:0px; margin:0px;"><h4>Write Ups</h4></div>
-        <div class = "box" style="margin:0px; margin-top:5px;">
+        <div class = "box" style="margin:0px; margin-top:5px;background-color:#f1f1f1;">
           <p>Write ups will go here<p>
         </div>
       </div>
